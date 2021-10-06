4586279e68ad43368696b8d6354702987e1ea3ae
TShields21 <Tiffany.Shields21@gmail.com>
2021-10-06 12:20:31 -0400

    Edited index.js so the page could compile properly. Added the sign-up background image/CSH icon.

---
+++ clinispanhealth-app/src/components/sign-up-header.js
@@ -0,0 +1,11 @@
+// This header is going to be commonly used amongst the login and sign-up portals for admin, patient, and partner.
+
+const signUpHeader = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default signUpHeader
