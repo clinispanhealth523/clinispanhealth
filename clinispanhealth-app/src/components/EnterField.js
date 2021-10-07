01bb791cc8e6d5bef21b4e894d0925724c4aa040
TShields21 <Tiffany.Shields21@gmail.com>
2021-10-07 13:18:32 -0400

    Finalized the sign-up page for patients. Additionally finalized the re-useable components such as the sign-up button and entry fields, for usage with admin and partner account creation.
    To-Do (Future):
    Link the sign up page with the 'browse studies', and 'Login' pages.

--- clinispanhealth-app/src/components/EnterField.js
+++ clinispanhealth-app/src/components/EnterField.js
@@ -1,9 +1,16 @@
-const EnterField = () => {
+// Requires different inputs (e.g. string, numerical, etc depending on the field)
+const EnterField = (props) => {
+    const type = props.forName == 'pw' || 'emailAddress' ? 'text' : 'number'
+    const placeholder = props.placeholder != null ? props.placeholder : ''
     return (
-        <div>
-            
+        <div className='form'>
+            <label forName={type}>{props.label}</label><br/>
+            <input type={type} placeholder={placeholder}></input>
         </div>
     )
 }
+
+
+
 
 export default EnterField
--- clinispanhealth-app/src/components/SignUpButton.js
+++ clinispanhealth-app/src/components/SignUpButton.js
@@ -1,11 +1,17 @@
 /* 
 Can be reused with every sign up and login page via admin, partner, or patient.
+The 'long' is in reference to the sign up button at the end of the form fields. the 'signUpButton' is in reference
+to the sign up button beside the 'login' hyperlink at the top of the page.
 */
 
-const SignUpButton = () => {
+const SignUpButton = ({className}) => {
+    const classN = className == 'long' ? 'long' : 'signUpButton'
+
     return (
-        <button className='signUpButton'>Sign Up</button>
+        <button className={classN}><b>Sign Up</b></button>
     )
 }
+
+
 
 export default SignUpButton
