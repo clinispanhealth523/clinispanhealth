62a8306f8a0a7472614e1e34ce59d8f80584fefc
kyruu <42990460+maatal22@users.noreply.github.com>
2021-10-12 14:42:19 -0400

    Changed NavButton to return just a <Link> component so that you can click anywhere inside the shaded box (not just the text) and be redirected.

--- clinispanhealth-app/src/components/Banner.js
+++ clinispanhealth-app/src/components/Banner.js
@@ -1,7 +1,5 @@
 import logo from '../images/CSH Logo.png'
-import { Link } from "react-router-dom";
 import NavButton from './NavButton';
-
 
 // 'text' is being used to show which button needs to be highlighted in the nav bar depending on the page.
 
@@ -10,17 +8,17 @@ const Banner = ({color}) => {
     return (
       <div className="navbar">
         <div id="left-navbar">
-          <NavButton content='Home' text={color === 'home' ? 'home' : ''} link='/home'/>
-          <NavButton content='Studies For You' text={color === 'studies-for-you' ? 'home' : ''} link='/studies-for-you'/>
-          <NavButton content='Browse Studies' text={color === 'browse-studies' ? 'home' : ''} link='/browse-studies'/>
-          <NavButton content='Manage Profile' text={color === 'manage-profile' ? 'home' : ''} link='/manage-profile'/>
-          <NavButton content='FAQ' text={color === 'faq' ? 'home' : ''} link='/faq'/>
+          <NavButton className="nav" content='Home' text={color === 'home' ? 'home' : 'na'} link='/home'/>
+          <NavButton className="nav" content='Studies For You' text={color === 'studies-for-you' ? 'home' : 'na'} link='/studies-for-you'/>
+          <NavButton className="nav" content='Browse Studies' text={color === 'browse-studies' ? 'home' : 'na'} link='/browse-studies'/>
+          <NavButton className="nav" content='Manage Profile' text={color === 'manage-profile' ? 'home' : 'na'} link='/manage-profile'/>
+          <NavButton className="nav" content='FAQ' text={color === 'faq' ? 'home' : 'na'} link='/faq'/>
         </div>
         <div id="right-navbar">
           <img src={logo} alt="logo" width="300" height="100"/>
         </div>  
       </div>
-    )
+    );
 }
 
 export default Banner
--- clinispanhealth-app/src/components/NavButton.js
+++ clinispanhealth-app/src/components/NavButton.js
@@ -2,12 +2,8 @@ import { Link } from "react-router-dom";
 
 const NavButton = ({content, text, link}) => {
     return ( 
-    <button className="nav">
-        <Link className='linkH' id={text === 'home' ? 'home' : 'na'} to={link}>{content}</Link>
-    </button>
-        
-
-    )
+    <Link className="nav" id={text === 'home' ? 'home' : 'na'} to={link}>{content}</Link>
+    );
 }
 
 export default NavButton
