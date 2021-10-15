0f2b476c763f52cefcea0bd61f6e8aa49c0e9470
ckoonce3 <ckoonce10@gmail.com>
2021-10-14 23:14:35 -0400

    Debugging

--- server/index.js
+++ server/index.js
@@ -2,17 +2,18 @@
 // server/index.js
 const path = require('path');
 const express = require('express');
+
 const app = express();
 
 // Have Node serve the files for our built React app
 app.use(express.static(path.resolve(__dirname, '../clinispanhealth-app/build')));
 
 // Handle GET requests to /api route
-app.get("/api", (req, res) => {
+app.get("/", (req, res) => {
   res.json({ message: "Hello from server!" });
 });
 
 // All other GET requests not handled before will return our React app
-app.get('*', (req, res) => {
+app.get('/app', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../clinispanhealth-app/build', 'index.html'));
 });
