7d13acc6d5583e85a0178fc03af80f4bbd482c38
ckoonce3 <ckoonce10@gmail.com>
2021-10-14 22:30:43 -0400

    Creating a server that automatically performs npm build

--- server/index.js
+++ server/index.js
@@ -1,15 +1,17 @@
 // server/index.js
+// server/index.js
+const path = require('path');
+const express = require('express');
 
-const express = require("express");
+// Have Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, '../clinispanhealth-app/build')));
 
-const PORT = process.env.PORT || 3001;
-
-const app = express();
-
+// Handle GET requests to /api route
 app.get("/api", (req, res) => {
-    res.json({ message: "Hello from server!" });
-  });
+  res.json({ message: "Hello from server!" });
+});
 
-app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
+// All other GET requests not handled before will return our React app
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../clinispanhealth-app/build', 'index.html'));
 });
