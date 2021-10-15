0a2f728e7688543d83d2067c470d240deec6ed60
ckoonce3 <ckoonce10@gmail.com>
2021-10-14 22:50:32 -0400

    Fixing error in server code

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@
 // server/index.js
 const path = require('path');
 const express = require('express');
+const app = express();
 
 // Have Node serve the files for our built React app
 app.use(express.static(path.resolve(__dirname, '../clinispanhealth-app/build')));
