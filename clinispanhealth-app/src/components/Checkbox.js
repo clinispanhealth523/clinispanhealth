d8e5d46c724b0c4a5aecaf5d347dda790e6e5d87
kyruu <42990460+maatal22@users.noreply.github.com>
2021-10-17 19:28:52 -0400

    Finalized layout of Manage Profile page

--- clinispanhealth-app/src/components/Checkbox.js
+++ clinispanhealth-app/src/components/Checkbox.js
@@ -1,15 +1,15 @@
 // Requires different inputs (e.g. string, numerical, etc depending on the field)
-const Input = (props) => {
+const Checkbox = (props) => {
     const type = props.forName === 'pw' || 'emailAddress' ? 'text' : 'number'
     // need to redo placeholder so that it 
     const placeholder = props.placeholder != null ? props.placeholder : ''
  
     return (
-        <div className='inputContainer'>
-            <label className="fieldLabel" forName={type}>{props.label}</label><br/>
-            <input className="input" type={type} placeholder={placeholder}></input>
+        <div className="checkboxContainer">
+            <input className="checkbox" type="checkbox" placeholder={placeholder}></input>
+            <label className="checkboxLabel" forName={type}>{props.label}</label><br/>
         </div>
     )
 }
 
-export default Input;
+export default Checkbox;
