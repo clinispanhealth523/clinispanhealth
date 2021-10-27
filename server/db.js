4fff5f834d7001b4c99d6f57297a0e22f5310e7f
ckoonce3 <ckoonce10@gmail.com>
2021-10-27 16:30:44 -0400

    Finalized the database schema creation and tested to make sure database was created without errors

--- server/db.js
+++ server/db.js
@@ -40,17 +40,39 @@ function SQLRead(sql) {
 
 // RESET THE DATABASE
 function resetDB() {
-    executeSQL('DROP TABLE IF EXISTS Patient');
-    executeSQL('DROP TABLE IF EXISTS Admin');
-    executeSQL('DROP TABLE IF EXISTS Partner');
-    executeSQL('DROP TABLE IF EXISTS Manager');
-    executeSQL('DROP TABLE IF EXISTS Study');
-    executeSQL('DROP TABLE IF EXISTS Condition');
-    createUserTables();
+    dropRelations();
+    dropEntities();
+    createEntities();
+    createRelations();
 }
 
-// CREATES THE INITIAL USER TABLES
-function createUserTables() {
+// Drops all entity tables
+function dropEntities() {
+    // Tables with foreign keys need to be removed first
+    executeSQL('DROP TABLE IF EXISTS Location');
+    executeSQL('DROP TABLE IF EXISTS Ethnicity');
+    executeSQL('DROP TABLE IF EXISTS Characteristic');
+    executeSQL('DROP TABLE IF EXISTS StudyCondition');
+    executeSQL('DROP TABLE IF EXISTS Study');
+    executeSQL('DROP TABLE IF EXISTS Manager');
+    executeSQL('DROP TABLE IF EXISTS Partner');
+    executeSQL('DROP TABLE IF EXISTS Admin');
+    executeSQL('DROP TABLE IF EXISTS Patient');  
+}
+
+// Drops all relation tables
+function dropRelations() {
+    executeSQL('DROP TABLE IF EXISTS HasEthnicity');
+    executeSQL('DROP TABLE IF EXISTS EnrolledIn');
+    executeSQL('DROP TABLE IF EXISTS InterestedIn');
+    executeSQL('DROP TABLE IF EXISTS HasCondition');
+    executeSQL('DROP TABLE IF EXISTS ConditionFor');
+    executeSQL('DROP TABLE IF EXISTS CharacteristicFor');
+    executeSQL('DROP TABLE IF EXISTS LocatedIn');
+}
+
+// CREATES THE INITIAL ENTITY TABLES IN THE DB
+function createEntities() {
 
     // Patient Table
     executeSQL('CREATE TABLE IF NOT EXISTS Patient (' +
@@ -102,8 +124,78 @@ function createUserTables() {
     );
 
     // Study Characteristic Table
+    executeSQL('CREATE TABLE IF NOT EXISTS Characteristic (' +
+        'id int PRIMARY KEY NOT NULL,' +
+        'name VARCHAR(255) UNIQUE NOT NULL)'
+    );
+
+    // Ethnicity Table
+    executeSQL('CREATE TABLE IF NOT EXISTS Ethnicity (' +
+        'id int PRIMARY KEY NOT NULL,' +
+        'name VARCHAR(255) UNIQUE NOT NULL)'
+    );
+
+    // Location Table
+    executeSQL('CREATE TABLE IF NOT EXISTS Location (' +
+        'zipcode int PRIMARY KEY NOT NULL,' +
+        'city VARCHAR(255),' +
+        'state CHAR(2))'
+    );
+
+    console.log("Entities Created");
+}
+
+// CREATES THE RELATIONAL TABLES IN SQL
+function createRelations() {
+    executeSQL('CREATE TABLE IF NOT EXISTS EnrolledIn (' +
+        'patient int,' +
+        'study int,' +
+        'FOREIGN KEY (patient) REFERENCES Patient(id),' +
+        'FOREIGN KEY (study) REFERENCES Study(nct))'
+    );
+
+    executeSQL('CREATE TABLE IF NOT EXISTS HasEthnicity (' +
+        'patient int,' +
+        'ethnicity int,' +
+        'FOREIGN KEY (patient) REFERENCES Patient(id),' +
+        'FOREIGN KEY (ethnicity) REFERENCES Ethnicity(id))'
+    );
 
 
+    executeSQL('CREATE TABLE IF NOT EXISTS InterestedIn (' +
+        'patient int,' +
+        'characteristic int,' +
+        'FOREIGN KEY (patient) REFERENCES Patient(id),' +
+        'FOREIGN KEY (characteristic) REFERENCES Characteristic(id))'
+    );
+
+    executeSQL('CREATE TABLE IF NOT EXISTS HasCondition (' +
+        'patient int,' +
+        'study_condition int,' +
+        'FOREIGN KEY (patient) REFERENCES Patient(id),' +
+        'FOREIGN KEY (study_condition) REFERENCES StudyCondition(id))'
+    );
+
+    executeSQL('CREATE TABLE IF NOT EXISTS ConditionFor (' +
+        'study int,' +
+        'study_condition int,' +
+        'FOREIGN KEY (study) REFERENCES Study(nct),' +
+        'FOREIGN KEY (study_condition) REFERENCES StudyCondition(id))'
+    );
+
+    executeSQL('CREATE TABLE IF NOT EXISTS CharacteristicFor(' +
+        'characteristic int,' +
+        'study int,' +
+        'FOREIGN KEY (characteristic) REFERENCES Characteristic(id),' +
+        'FOREIGN KEY (study) REFERENCES Study(nct))'
+    );
+
+    executeSQL('CREATE TABLE IF NOT EXISTS LocatedIn(' +
+        'study int,' +
+        'location int,' +
+        'FOREIGN KEY (study) REFERENCES Study(nct),' +
+        'FOREIGN KEY (location) REFERENCES Location(zipcode))'
+    );
 }
 
 // INSERTS A ROW INTO THE PATIENT TABLE from the create profile
@@ -122,7 +214,4 @@ function getPatients() {
 createPatient('jdoe@gmail.com','puppylover123','27514','9191234567');
 getPatients();
 */
-
-createUserTables();
-
 
